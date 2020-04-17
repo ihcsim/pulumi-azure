343911b0cfb5b67cde155624e5dc6c822e1f181f
ihcsim <ihcsim@gmail.com>
2020-04-16 21:05:49 -0700

    Add compute resources
    Signed-off-by: ihcsim <ihcsim@gmail.com>

--- cmd/pulumi/main.go
+++ cmd/pulumi/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ihcsim/pulumi-azure/v2/config"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/compute"
 	"github.com/pulumi/pulumi-azure/sdk/go/azure/core"
 	"github.com/pulumi/pulumi-azure/sdk/go/azure/network"
 	"github.com/pulumi/pulumi/sdk/go/pulumi"
@@ -19,7 +22,12 @@ func main() {
 			return err
 		}
 
-		if _, err := createVirtualNetworks(ctx, resourceGroup, commonTags); err != nil {
+		virtualNetworks, err := createVirtualNetworks(ctx, resourceGroup, commonTags)
+		if err != nil {
+			return err
+		}
+
+		if _, err := runCompute(ctx, resourceGroup, virtualNetworks, commonTags); err != nil {
 			return err
 		}
 
@@ -132,4 +140,153 @@ func createVirtualNetworks(
 	}
 
 	return networks, nil
+}
+
+func runCompute(
+	ctx *pulumi.Context,
+	resourceGroup *core.ResourceGroup,
+	virtualNetworks []*network.VirtualNetwork,
+	tags pulumi.StringMap) ([]*compute.VirtualMachine, error) {
+
+	vms := []*compute.VirtualMachine{}
+	for _, virtualNetwork := range virtualNetworks {
+		for _, vmConfig := range config.VirtualMachines {
+			useNetwork := false
+			virtualNetwork.Name.ApplyBool(func(name string) bool {
+				useNetwork = name == string(vmConfig.VirtualNetwork)
+				return useNetwork
+			})
+
+			if useNetwork {
+				continue
+			}
+
+			subnetID := virtualNetwork.Subnets.ApplyString(func(subnets []network.VirtualNetworkSubnet) string {
+				for _, subnet := range subnets {
+					if subnet.Name == string(vmConfig.Subnet) {
+						return *subnet.Id
+					}
+				}
+				return ""
+			})
+
+			netInf, err := createPrimaryNetworkInterface(ctx, vmConfig.Name, subnetID, resourceGroup, tags)
+			if err != nil {
+				return nil, err
+			}
+
+			osProfileConfig, exists := config.OSProfiles[vmConfig.OSProfile]
+			if !exists {
+				return nil, missingConfigErr{vmConfig.OSProfile, "osprofile"}
+			}
+
+			osProfile := compute.VirtualMachineOsProfileArgs{
+				AdminPassword: &osProfileConfig.AdminPassword,
+				AdminUsername: osProfileConfig.AdminUsername,
+				ComputerName:  vmConfig.Name,
+				CustomData:    osProfileConfig.CustomData,
+			}
+
+			osProfileLinuxConfig, exists := config.OSProfileLinux[vmConfig.OSProfileLinux]
+			if !exists {
+				return nil, missingConfigErr{vmConfig.OSProfileLinux, "osprofile-linux"}
+			}
+
+			osProfileLinux := compute.VirtualMachineOsProfileLinuxConfigArgs{
+				DisablePasswordAuthentication: osProfileLinuxConfig.DisablePasswordAuthentication,
+				SshKeys: compute.VirtualMachineOsProfileLinuxConfigSshKeyArray{
+					compute.VirtualMachineOsProfileLinuxConfigSshKeyArgs{
+						KeyData: osProfileLinuxConfig.SSHKeyData,
+						Path:    osProfileLinuxConfig.SSHKeyPath,
+					},
+				},
+			}
+
+			osDiskConfig, exists := config.StorageOSDisks[vmConfig.StorageOSDisk]
+			if !exists {
+				return nil, missingConfigErr{vmConfig.StorageOSDisk, "osdisk"}
+			}
+
+			osDisk := compute.VirtualMachineStorageOsDiskArgs{
+				CreateOption: osDiskConfig.CreateOption,
+				DiskSizeGb:   osDiskConfig.DiskSizeGb,
+				Name:         vmConfig.Name,
+				OsType:       osDiskConfig.OSType,
+			}
+
+			imageRefConfig, exists := config.StorageImageReference[vmConfig.StorageImageReference]
+			if !exists {
+				return nil, missingConfigErr{vmConfig.StorageImageReference, "image-reference"}
+			}
+
+			imageRef := compute.VirtualMachineStorageImageReferenceArgs{
+				Offer:     imageRefConfig.Offer,
+				Publisher: imageRefConfig.Publisher,
+				Sku:       imageRefConfig.SKU,
+				Version:   imageRefConfig.Version,
+			}
+
+			vm, err := compute.NewVirtualMachine(ctx, string(vmConfig.Name), &compute.VirtualMachineArgs{
+				Location:                  resourceGroup.Location,
+				Name:                      vmConfig.Name,
+				OsProfile:                 osProfile,
+				OsProfileLinuxConfig:      osProfileLinux,
+				PrimaryNetworkInterfaceId: netInf.ID(),
+				NetworkInterfaceIds:       pulumi.StringArray{netInf.ID()},
+				StorageImageReference:     imageRef,
+				ResourceGroupName:         resourceGroup.Name,
+				StorageOsDisk:             osDisk,
+				Tags:                      tags,
+				VmSize:                    vmConfig.VMSize,
+			})
+			if err != nil {
+				return nil, err
+			}
+
+			vms = append(vms, vm)
+		}
+	}
+
+	return vms, nil
+}
+
+func createPrimaryNetworkInterface(
+	ctx *pulumi.Context,
+	virtualMachine pulumi.String,
+	subnetID pulumi.StringOutput,
+	resourceGroup *core.ResourceGroup,
+	tags pulumi.StringMap) (*network.NetworkInterface, error) {
+
+	for netInfKind, netInfConfig := range config.NetworkInterfaces {
+		if netInfKind != config.NetworkInterfaceKindPrimary {
+			continue
+		}
+
+		ipConfigs := network.NetworkInterfaceIpConfigurationArray{}
+		for ipConfigKind, ipConfigData := range config.IPConfiguration {
+			if ipConfigKind != netInfConfig.IPConfiguration || !ipConfigData.Primary {
+				continue
+			}
+
+			ipConfigs = append(ipConfigs, network.NetworkInterfaceIpConfigurationArgs{
+				Name:                       pulumi.Sprintf("%s-primary-ipconfig", virtualMachine),
+				Primary:                    ipConfigData.Primary,
+				PrivateIpAddressAllocation: ipConfigData.PrivateIPAddressAllocation,
+				PrivateIpAddressVersion:    ipConfigData.PrivateIPAddressVersion,
+				SubnetId:                   subnetID,
+			})
+		}
+
+		args := &network.NetworkInterfaceArgs{
+			IpConfigurations:  ipConfigs,
+			Location:          resourceGroup.Location,
+			ResourceGroupName: resourceGroup.Name,
+			Tags:              tags,
+		}
+
+		netInfName := fmt.Sprintf("%s-primary", virtualMachine)
+		return network.NewNetworkInterface(ctx, netInfName, args)
+	}
+
+	return nil, missingConfigErr{virtualMachine, "primary network interface"}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module github.com/ihcsim/pulumi-azure/v2
 go 1.13
 
 require (
+	github.com/Azure/azure-sdk-for-go v41.2.0+incompatible
+	github.com/Azure/go-autorest/autorest v0.10.0 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.8.3 // indirect
+	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
 	github.com/pulumi/pulumi-azure/sdk v0.0.0-20200326135512-7563f3e5f0db
 	github.com/pulumi/pulumi/sdk v0.0.0-20200327215558-fa3d50faa2ea
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc // indirect
+	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
+	golang.org/x/tools v0.0.0-20200410194907-79a7a3126eef // indirect
 )
